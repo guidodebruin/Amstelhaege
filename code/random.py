21542ecd59cc0e89734b62b5bcb2bf3612d52b42
Guido de Bruin <guidodebruin@MacBook-Pro-van-Guido.local>
2021-01-12 16:33:03 +0100

    Calculations

--- code/random.py
+++ code/random.py
@@ -4,9 +4,25 @@ from bungalow import Bungalow
 from maison import Maison
 
 class Random():
-
     def place_house(self, coordinates):
         pass
 
-    def return_coordinates(self ):
-        pass
+    def return_coordinates(self):
+        # Singlehouse (x,y)
+        # lowerleft coordinates = (0, -8)
+        # upperright coordinates = (+8, 0)
+        # lowerright coordinates = (+8, -8)
+
+        # Bungalow (x,y)
+        # get random upperleft coordinates
+        # lowerleft coordinates = (0, -7)
+        # upperright coordinates = (+11, 0)
+        # lowerright coordinates = (+11, -7)
+
+        # Maison (x,y)
+        # get random upperleft coordinates
+        # lowerleft coordinates = (-10, 0)
+        # upperright coordinates = (0, 12)
+        # lowerright coordinates = (-10, 12)
+
+
