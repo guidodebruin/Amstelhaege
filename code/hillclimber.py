d79072c48a2bba4805d2117bcb51b6d76ebfb686
Guido de Bruin <guidodebruin@MacBook-Pro-van-Guido.local>
2021-01-19 10:08:50 +0100

    Hill Climber

---
+++ code/hillclimber.py
@@ -0,0 +1,34 @@
+class House():
+
+    def random_house(self, houses):
+      """
+         Returns random a random house object
+      """
+    random_house = random.choice(houses))
+
+    return random_house
+    
+    
+    def random_direction(self):
+      """
+         Returns random a random direction
+      """
+    # return direction
+        # Check if random direction doesn't overlap with other house or water
+
+    
+    def price_comparison(self, new_price, old_price)
+
+      """
+         Returns random a random direction
+      """
+
+        if new_price > old_price:
+            return True
+        
+        return False
+     
+
+
+
+    
